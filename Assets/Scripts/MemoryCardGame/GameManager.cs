2594328b8711f0c482ccaf1955239551a671a1fd
agnieszkazielke <agnieszka.zielke@gmail.com>
2022-10-23 15:42:08 +0100

    update

--- Assets/Scripts/MemoryCardGame/GameManager.cs
+++ Assets/Scripts/MemoryCardGame/GameManager.cs
@@ -1,27 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.Assertions;
 using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
-    public UnityEvent onCardsChecked;
     
     [SerializeField] private Logger _debugLogger;
-    
-    [SerializeField] private MemoryCard _openCard1;
-    [SerializeField] private MemoryCard _openCard2;
+    [SerializeField] private List<Texture> _cardImageTextures = new List<Texture>();
+    [SerializeField] private List<Texture> _selectedCardImageTextures = new List<Texture>();
+    [SerializeField] private MemoryCard[] _memoryCards;
+    private MemoryCard _openCard1;
+    private MemoryCard _openCard2;
+    private System.Random _random = new System.Random();  
 
     private void Awake()
     {
         Instance = this;
+        _memoryCards = FindObjectsOfType<MemoryCard>();
     }
 
     void Start()
     {
+        Assert.IsTrue(_memoryCards.Length <= _cardImageTextures.Count * 2, "Error: You don't have enough textures");
+        Assert.IsTrue(_memoryCards.Length % 2 == 0, "Error: You need an even amount of memory cards");
         
+        ShuffleCards(_cardImageTextures);
+        SelectCardImageTextures();
+        ShuffleCards(_selectedCardImageTextures);
+        ApplyTexturesToCards();
+      
     }
+
+    private void ShuffleCards(List<Texture> list)  
+    {  
+        // Fisher-Yates shuffle
+        int n = list.Count;  
+        while (n > 1) {  
+            n--;  
+            int k = _random.Next(n + 1);  
+            Texture value = list[k];
+            list[k] = list[n];  
+            list[n] = value;  
+        }  
+    }
+    
+    private void SelectCardImageTextures()
+    {
+        for (int i = 0; i < _memoryCards.Length / 2; i++)
+        {
+            _selectedCardImageTextures.Add(_cardImageTextures[i]);
+            _selectedCardImageTextures.Add(_cardImageTextures[i]);
+        }
+    }
+
+    private void ApplyTexturesToCards()
+    {
+        for (int i = 0; i < _memoryCards.Length; i++)
+        {
+            _memoryCards[i].revealImage.texture = _selectedCardImageTextures[i];
+        }
+    }
+    
 
     public void OpenCard(MemoryCard card)
     {
@@ -40,7 +83,6 @@ public class GameManager : MonoBehaviour
             CheckCards();
         }
         
-       
     }
 
     private void CheckCards()
@@ -49,16 +91,16 @@ public class GameManager : MonoBehaviour
         
         if (_openCard1.revealImage.texture == _openCard2.revealImage.texture)
         {
-            _debugLogger.LogInfo("Cards match!");  
+            _debugLogger.LogInfo("Cards match!");
+            _openCard1.RemoveCard();
+            _openCard2.RemoveCard();
         }
 
         else
         {
             _debugLogger.LogInfo("Cards don't match!"); 
-            onCardsChecked.Invoke();
-            
-            //_openCard1.CoverCard();
-            //_openCard2.CoverCard();
+            _openCard1.CoverCard();
+            _openCard2.CoverCard();
         }
 
         _openCard1 = null;
@@ -67,7 +109,7 @@ public class GameManager : MonoBehaviour
         
 
     }
-
+    
     
     
     // Update is called once per frame
--- Assets/Scripts/MemoryCardGame/MemoryCard.cs
+++ Assets/Scripts/MemoryCardGame/MemoryCard.cs
@@ -6,17 +6,14 @@ using UnityEngine.UI;
 
 public class MemoryCard : MonoBehaviour
 {
-    public RawImage revealImage;
-
-    [SerializeField] private MemoryCardSwipe _cardSwipe;
-
-    public bool _cardOpen = false;
     
+    public RawImage revealImage;
+    
+    [SerializeField] private MemoryCardSwipe _cardSwipe;
+    [SerializeField] private Logger _debugLogger;
 
-    private void Start()
-    {
-        GameManager.Instance.onCardsChecked.AddListener(CoverCard);
-    }
+    private bool _cardOpen = false;
+    
 
     public void RevealCard()
     {
@@ -27,13 +24,36 @@ public class MemoryCard : MonoBehaviour
 
     public void CoverCard()
     {
+        _debugLogger.LogInfo("CoverCard() executed on " + gameObject.name);
         _cardOpen = false;
         _cardSwipe.ScrollBack();
-        
+        StartCoroutine(SetFalse());
+
     }
+    
+    // no idea why this is needed but it is needed to make sure all cards get set to false for some reason
+    private IEnumerator SetFalse()
+    {
+        yield return new WaitForSeconds(2f);
+        _cardOpen = false;
+
+    }
+
+    public void RemoveCard()
+    {
+        StartCoroutine(DestroyCard());
+    }
+    
+    private IEnumerator DestroyCard()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+
+    }
+
 
     private void OnDestroy()
     {
-        GameManager.Instance.onCardsChecked.RemoveAllListeners();
+        
     }
 }
